f3991d3171c9a7cc251759a29607f86860918ded
AustinEngineer <austinfairyland@yeah.net>
2024-01-12 11:57:43 +0800

    :sparkles: init commit

---
+++ docs/.vitepress/configs/nav.ts
@@ -0,0 +1,7 @@
+import type {DefaultTheme} from 'vitepress'
+
+export const nav: DefaultTheme.Config['nav'] = [
+  {text: 'CSDN博客', link: 'https://alicehome.blog.csdn.net'},
+  {text: '博客园', link: 'https://www.cnblogs.com/alicehome'},
+  {text: '衣带渐宽终不悔, 为伊消得人憔悴', link: ''}
+]
